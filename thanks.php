97d1460f88f62949af5907f8e1fbe19ca0bf1001
北岡徹平 <teppei997@gmail.com>
2020-06-04 16:54:13 +0900

    データ登録完成

--- thanks.php
+++ thanks.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once('functions.php');
+require_once('dbconnect.php');
 // var_dump($_POST);
 //入力された内容を取得
 $username = $_POST['username'];
@@ -15,6 +16,16 @@ $email = $_POST['email'];
 $content = $_POST['content'];
 // echo $content;
 // echo '<br>';
+//実行するSQL文の作成
+$sql = 
+  'INSERT INTO surveys(name, email, content, created_at) VALUES(:name, :email, :content, now())';
+// SQL文の実行準備
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(':name', $username, PDO::PARAM_STR);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':content', $content, PDO::PARAM_STR);
+// SQL文を実行
+$stmt->execute();
 
 
 
